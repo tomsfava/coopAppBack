ab3163c32e728954aadc639e5a098b2c0af48d84
Tomás <tomsfava@gmail.com>
2025-08-26 19:21:53 -0300

    feat: new algorithm architecture proposal

--- common/services/distribution.py
+++ common/services/distribution.py
@@ -1,19 +1,39 @@
 from decimal import Decimal  # noqa: F401
 
-from operations.models import Offer, Order
 
-
-def run_distribution(orders: list[Order], offers: list[Offer]) -> list[dict]:
+def distribute(orders: list[dict], offers: list[dict]) -> list[dict]:
     """
-    Recebe listas de Orders e Offers.
+    Recebe listas de Orders e Offers serializados.
     Retorna uma lista de propostas de distribuição (JSON serializável).
     """
     pass
 
 
-def redistribute(proposal_json_list: list[dict]) -> list[dict]:
+def redistribute(
+    original_proposal_json_list: list[dict], altered_proposal_json_list: list[dict]
+) -> list[dict]:
     """
-    Recebe uma lista de propostas de distribuição (JSON serializável).
+    Recebe uma lista de propostas de distribuição (JSON serializável) alterada.
     Retorna uma nova lista com base em ajustes manuais da proposta inicial.
     """
     pass
+
+
+def fairness(proposal_json_list) -> list[dict]:
+    """
+    Recebe uma lista de propostas de distribuição (JSON serializável).
+    Retorna uma nova lista ajustada para garantir o máximo de equidade.
+    """
+    pass
+
+
+def run_distribute(orders: list[dict], offers: list[dict]) -> list[dict]:
+    proposal = distribute(orders, offers)
+    return fairness(proposal)
+
+
+def run_redistribute(
+    original_proposal_json_list: list[dict], altered_proposal_json_list: list[dict]
+) -> list[dict]:
+    proposal = redistribute(original_proposal_json_list, altered_proposal_json_list)
+    return fairness(proposal)
